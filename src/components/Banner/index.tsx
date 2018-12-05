a184cfe6da0bfedefb307876cb4a1d016f219532
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-12-05 19:03:22 +0800

    update npm

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,48 +1,6 @@
 import * as React from "react";
-import { Box, Flex, Text } from "src/theme/primitives";
-import { Image } from "../Image";
-import { styled } from "src/theme";
-
-const Wrapper = styled(Flex)`
-  position: relative;
-  overflow: hidden;
-  min-height: 300px;
-  align-items: flex-end;
-`;
-
-const ImageWrapper = styled(Box)`
-  position: absolute;
-  height: 100%;
-  width: 100%;
-  top: 0;
-  left: 0;
-`;
-
-const Img = styled(Image)`
-  height: 100%;
-  width: 100%;
-`;
-
-const Content = styled(Flex)`
-  z-index: 2;
-  position: relative;
-  border-color: ${props => props.theme.colors.primary.main};
-  &::after {
-    z-index: -1;
-    position: absolute;
-    border-top: ${props => props.theme.borders[1]};
-    border-color: ${props => props.theme.colors.white.light};
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    content: "";
-    background: linear-gradient(to top,
-      rgba(255,255,255,1),
-      rgba(255,255,255,0.95) 30%,
-      rgba(255,255,255,0.56));
-  }
-`;
+import { Box, Text } from "src/theme/primitives";
+import { BannerWrapper } from "./BannerWrapper";
 
 interface BannerProps {
   heading: React.ReactNode;
@@ -52,22 +10,12 @@ interface BannerProps {
 
 const Banner: React.SFC<BannerProps> = (props) => {
   return (
-    <Wrapper>
-      <ImageWrapper>
-        <Img fluid={props.image}/>
-      </ImageWrapper>
-      <Content
-        width={1}
-        px={3}
-        py={4}
-        mt={4}
-        flexDirection="column"
-        alignItems="center"
-      >
+    <BannerWrapper image={props.image}>
+      <Box p={3}>
         <Text
           as="h1"
-          fontSize={5}
-          color="primary.main"
+          fontSize={4}
+          color="white.main"
           fontWeight={2}
           textAlign="center"
           textTransform="uppercase"
@@ -81,8 +29,8 @@ const Banner: React.SFC<BannerProps> = (props) => {
             {props.subheading}
           </Text>
         }
-      </Content>
-    </Wrapper>
+      </Box>
+    </BannerWrapper>
   );
 };
 
