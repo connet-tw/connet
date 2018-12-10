864ade41452a755e63ca609e35f147d3556b792c
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-12-10 17:26:54 +0800

    create container

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -4,6 +4,7 @@ import { Image } from "../Image";
 import { Section, SectionHeader } from "../Section";
 import { Button } from "../Button";
 import { Box, Card, Flex, Text } from "src/theme/primitives";
+import { Container } from "../Container";
 
 interface CategoryLink {
   label: React.ReactNode;
@@ -29,39 +30,41 @@ const Categories: React.SFC<Props> = ({ image, gradient, heading, subheading, bo
       subheading={subheading}
       body={body}
     />
-    <Flex p={2} flexWrap="wrap">
-      {categoryLinks.map((x) =>
-        <Flex width={[1, 1/2, 1/2, 1/4]} p={2}>
-          <Card
-            radius={2}
-            width={1}
-            key={x.to}
-            shadow={1}
-            flexDirection="column"
-            bg="background.paper"
-          >
-            <Flex flexDirection="column" style={{height: "100%"}}>
-              <Image style={{height: 140}} fluid={x.image}/>
-              <Box m={3} style={{flexGrow: 1}}>
-                <Text as="h3" color="primary.main" fontWeight={4} fontSize={3}>
-                  {x.label}
-                </Text>
-                {x.text &&
-                  <Text mt={2} as="p">
-                    {x.text}
+    <Container>
+      <Flex p={2} flexWrap="wrap">
+        {categoryLinks.map((x) =>
+          <Flex width={[1, 1/2, 1/2, 1/4]} p={2}>
+            <Card
+              radius={2}
+              width={1}
+              key={x.to}
+              shadow={1}
+              flexDirection="column"
+              bg="background.paper"
+            >
+              <Flex flexDirection="column" style={{height: "100%"}}>
+                <Image style={{height: 140}} fluid={x.image}/>
+                <Box m={3} style={{flexGrow: 1}}>
+                  <Text as="h3" color="primary.main" fontWeight={4} fontSize={3}>
+                    {x.label}
                   </Text>
-                }
-              </Box>
-              <Flex m={3}>
-                <Button as={Link} to={x.to} outlined>
-                  {x.buttonText}
-                </Button>
+                  {x.text &&
+                    <Text mt={2} as="p">
+                      {x.text}
+                    </Text>
+                  }
+                </Box>
+                <Flex m={3}>
+                  <Button as={Link} to={x.to} outlined>
+                    {x.buttonText}
+                  </Button>
+                </Flex>
               </Flex>
-            </Flex>
-          </Card>
-        </Flex>,
-      )}
+            </Card>
+          </Flex>,
+        )}
     </Flex>
+    </Container>
   </Section>
 );
 
