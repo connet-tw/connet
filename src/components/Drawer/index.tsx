cbf018fc023d704e09893ad7a2ca30c842f326a7
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-12-11 12:15:30 +0800

    animate drawer

--- src/components/Drawer/index.tsx
+++ src/components/Drawer/index.tsx
@@ -1,70 +1,78 @@
-import * as React from "react";
-import styled, { keyframes } from "styled-components";
+import * as React from 'react'
+import styled from 'styled-components'
+import { Motion, spring } from 'react-motion'
 
 interface DrawerProps {
-  anchor: "left" | "right" | "top" | "bottom";
-  width: number;
-  open: boolean;
-  handleClose(): void;
-  toggleMenu(): void;
+  anchor: 'left' | 'right' | 'top' | 'bottom'
+  width: number
+  open: boolean
+  handleClose(): void
+  toggleMenu(): void
 }
 
-const fadeIn = keyframes`
-from {
-  opacity: 0;
-}
-to {
-  opacity: 1;
-}
-`;
-
-const DrawerWrapper = styled.div<{width: number, open: boolean}>`
+const DrawerWrapper = styled.div<{ width: number; open: boolean }>`
   z-index: 1400;
   position: absolute;
   width: ${props => props.width}px;
   top: 0;
   right: 0;
   display: block;
-  transform: ${props => props.open ? "translateX(0)" : "translateX(100%)"};
-  transition: transform 0.3s ease-out;
-`;
+`
 
-const DrawerOverlay = styled.div<{open: boolean, onClick(): void}>`
+const DrawerOverlay = styled.div<{ open: boolean; onClick(): void }>`
   z-index: 1400;
   position: fixed;
   top: 0;
   right: 0;
   left: 0;
   bottom: 0;
-  display: ${props => props.open ? "auto" : "none"};
-  background: rgba(0,0,0,0.4);
-  animation: ${fadeIn} 0.3s ease-out;
-`;
+  background: rgba(0, 0, 0, 0.4);
+`
 
-const DrawerContent = styled.div<{open: boolean}>`
+const DrawerContent = styled.div<{ open: boolean }>`
   z-index: 1400;
-  position: fixed;
   height: 100vh;
   width: 100%;
   overflow-x: hidden;
-`;
+`
 
-const Drawer: React.SFC<DrawerProps> = ({width, open, toggleMenu, handleClose, children}) => {
+const Drawer: React.SFC<DrawerProps> = ({
+  width,
+  open,
+  toggleMenu,
+  handleClose,
+  children,
+}) => {
   return (
     <>
-      <DrawerOverlay open={open} onClick={handleClose}/>
-      <DrawerWrapper
-        open={open}
-        width={width}
+      <Motion
+        defaultStyle={{ x: 300, opacity: 1 }}
+        style={{ x: spring(open ? 0 : 300), opacity: spring(open ? 1 : 0) }}
       >
-        <DrawerContent open={open}>
-          {children}
-        </DrawerContent>
-      </DrawerWrapper>
+        {style => (
+          <>
+            <DrawerOverlay
+              style={{
+                display: open ? 'block' : 'none',
+                opacity: style.opacity,
+              }}
+              open={open}
+              onClick={handleClose}
+            />
+            <DrawerWrapper
+              style={{
+                transform: `translateX(${style.x}px)`,
+              }}
+              open={open}
+              width={width}
+            >
+              <DrawerContent open={open}>{children}</DrawerContent>
+            </DrawerWrapper>
+          </>
+        )}
+      </Motion>
     </>
-  );
+  )
 }
 
-export {
-  Drawer
-};
+export { Drawer }
