50766dac37bcf2d66a38b64cce5e2ecacf6c785e
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-12-11 14:41:42 +0800

    fix Logo click in drawer menu

--- src/components/DrawerMenu/index.tsx
+++ src/components/DrawerMenu/index.tsx
@@ -1,17 +1,17 @@
-import * as React from "react";
-import { MakeMenu } from "../utils/MakeMenu";
-import { Drawer } from "../Drawer";
-import { MenuButton } from "./MenuButton";
-import { CloseButton } from "./CloseButton";
-import { Flex, Text } from "src/theme/primitives";
-import { Button } from "../Button";
-import { Image } from "../Image";
-import { styled } from "src/theme";
-import { Link } from "../../i18n";
+import * as React from 'react'
+import { MakeMenu } from '../utils/MakeMenu'
+import { Drawer } from '../Drawer'
+import { MenuButton } from './MenuButton'
+import { CloseButton } from './CloseButton'
+import { Flex, Text } from 'src/theme/primitives'
+import { Button } from '../Button'
+import { Image } from '../Image'
+import { styled } from 'src/theme'
+import { Link } from '../../i18n'
 
 const Logo = styled(Image)`
   margin: 0 auto;
-`;
+`
 
 const DrawerContent = styled(Flex)`
   height: 100vh;
@@ -20,21 +20,21 @@ const DrawerContent = styled(Flex)`
 `
 
 interface DrawerMenuProps {
-  logo?: any;
-  title?: React.ReactNode;
-  navItems: {to: string, label: React.ReactNode}[];
+  logo?: any
+  title?: React.ReactNode
+  navItems: { to: string; label: React.ReactNode }[]
 }
 
 const DrawerMenu: React.SFC<DrawerMenuProps> = ({ logo, title, navItems }) => {
   return (
     <MakeMenu>
-      {injected =>
+      {injected => (
         <>
-          <MenuButton onClick={injected.toggleMenu}/>
+          <MenuButton onClick={injected.toggleMenu} />
 
           <Drawer
             open={injected.open}
-            anchor={"right"}
+            anchor={'right'}
             handleClose={injected.handleClose}
             toggleMenu={injected.toggleMenu}
             width={300}
@@ -45,40 +45,40 @@ const DrawerMenu: React.SFC<DrawerMenuProps> = ({ logo, title, navItems }) => {
               spacing={3}
             >
               <Flex justifyContent="flex-end">
-                <CloseButton onClick={injected.handleClose}/>
+                <CloseButton onClick={injected.handleClose} />
               </Flex>
-              {logo &&
+              {logo && (
                 <Flex justifyContent="center">
-                  <Link to="/">
-                    <Logo fixed={logo}/>
+                  <Link to="/" onClick={injected.handleClose}>
+                    <Logo fixed={logo} />
                   </Link>
                 </Flex>
-              }
-              {title &&
-                <Text as="h3"
-                  fontSize={3}
-                  textAlign="center"
-                >
-                  {title} 
+              )}
+              {title && (
+                <Text as="h3" fontSize={3} textAlign="center">
+                  {title}
                 </Text>
-              }
+              )}
               <Flex justifyContent="center" flexDirection="column" p={1}>
-                {navItems.map((x) => 
+                {navItems.map(x => (
                   <Flex key={x.to} p={1}>
-                    <Button onClick={injected.handleClose} width={1} to={x.to} as={Link}>
+                    <Button
+                      onClick={injected.handleClose}
+                      width={1}
+                      to={x.to}
+                      as={Link}
+                    >
                       {x.label}
                     </Button>
                   </Flex>
-                )}
+                ))}
               </Flex>
             </DrawerContent>
           </Drawer>
         </>
-      }
+      )}
     </MakeMenu>
-  );
+  )
 }
 
-export {
-  DrawerMenu
-};
+export { DrawerMenu }
