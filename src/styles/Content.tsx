52dce83befc2963a9cd50333fb2642182cc3b38e
Pawel Lisewski <pawel.lisewski@gmail.com>
2019-01-03 15:49:59 +0800

    update Content

--- src/styles/Content.tsx
+++ src/styles/Content.tsx
@@ -1,8 +1,12 @@
-import { styled, Flex } from "primithemes";
+import { styled, Box } from "primithemes";
 
-export const Content = styled(Flex)`
+export const Content = styled(Box)`
   font-family: ${props => props.theme.fonts.sans};
   color: ${props => props.theme.colors.text.dark};
+  text-align: center;
+  ${props => props.theme.devices[2]} {
+    text-align: center;
+  }
   & * {
     margin: 0;
     padding: 0;
@@ -20,7 +24,6 @@ export const Content = styled(Flex)`
   & p {
     font-weight: ${props => props.theme.fontWeights[3]};
     font-size: ${props => props.theme.fontSizes[2]};
-    text-align: center;
     margin: ${props => props.theme.sizes[3]} 0;
     line-height: ${props => props.theme.lineHeights[2]};
   }
