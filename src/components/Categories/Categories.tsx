a674e052feba43d978d95df6ed63617aaa53a2fb
Pawel Lisewski <pawel.lisewski@gmail.com>
2019-01-18 15:12:43 +0800

    add reaveal animation to categories

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -6,6 +6,7 @@ import { Box, Card, Flex, Text } from "primithemes";
 import { Container } from "../Container";
 import { Link } from "../../i18n";
 import { Content } from "../../styles/Content";
+import { FadeIn } from "../Reveal";
 
 interface CategoryLink {
   label: React.ReactNode;
@@ -51,38 +52,40 @@ const Categories: React.SFC<Props> = ({
         <Flex p={2} flexWrap="wrap">
           {categoryLinks.map((x, i) => (
             <Flex key={i} w={[1, 1 / 2, 1 / 2, 1 / 4]} p={2}>
-              <Card
-                radius={2}
-                w={1}
-                key={x.to}
-                shadow={1}
-                flexDirection="column"
-                bg="background.light"
-              >
-                <Flex flexDirection="column" style={{ height: "100%" }}>
-                  <Image style={{ height: 140 }} fluid={x.image} />
-                  <Box m={3} style={{ flexGrow: 1 }}>
-                    <Text
-                      is="h3"
-                      color="primary.main"
-                      fontWeight={4}
-                      fontSize={3}
-                    >
-                      {x.label}
-                    </Text>
-                    {x.text && (
-                      <Text mt={2} is="p">
-                        {x.text}
+              <FadeIn once style={{ width: "100%" }}>
+                <Card
+                  radius={2}
+                  w={1}
+                  key={x.to}
+                  shadow={1}
+                  flexDirection="column"
+                  bg="background.light"
+                >
+                  <Flex flexDirection="column" style={{ height: "100%" }}>
+                    <Image style={{ height: 140 }} fluid={x.image} />
+                    <Box m={3} style={{ flexGrow: 1 }}>
+                      <Text
+                        is="h3"
+                        color="primary.main"
+                        fontWeight={4}
+                        fontSize={3}
+                      >
+                        {x.label}
                       </Text>
-                    )}
-                  </Box>
-                  <Flex m={3}>
-                    <Link to={x.to}>
-                      <Button outlined>{x.buttonText}</Button>
-                    </Link>
+                      {x.text && (
+                        <Text mt={2} is="p">
+                          {x.text}
+                        </Text>
+                      )}
+                    </Box>
+                    <Flex m={3}>
+                      <Link to={x.to}>
+                        <Button outlined>{x.buttonText}</Button>
+                      </Link>
+                    </Flex>
                   </Flex>
-                </Flex>
-              </Card>
+                </Card>
+              </FadeIn>
             </Flex>
           ))}
         </Flex>
