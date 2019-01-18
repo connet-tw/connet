a0cbf674534723dfb9809396081d48f8508cbdc7
Pawel Lisewski <pawel.lisewski@gmail.com>
2019-01-18 16:39:51 +0800

    replace motion with transition

--- src/components/Drawer/index.tsx
+++ src/components/Drawer/index.tsx
@@ -1,13 +1,13 @@
-import * as React from 'react'
-import styled from 'styled-components'
-import { Motion, spring } from 'react-motion'
+import * as React from "react";
+import styled from "styled-components";
+import { Transition } from "react-spring";
 
 interface DrawerProps {
-  anchor: 'left' | 'right' | 'top' | 'bottom'
-  width: number
-  open: boolean
-  handleClose(): void
-  toggleMenu(): void
+  anchor: "left" | "right" | "top" | "bottom";
+  width: number;
+  open: boolean;
+  handleClose(): void;
+  toggleMenu(): void;
 }
 
 const DrawerWrapper = styled.div<{ width: number }>`
@@ -17,7 +17,7 @@ const DrawerWrapper = styled.div<{ width: number }>`
   top: 0;
   right: 0;
   display: block;
-`
+`;
 
 const DrawerOverlay = styled.div<{ onClick(): void }>`
   z-index: 1400;
@@ -28,59 +28,55 @@ const DrawerOverlay = styled.div<{ onClick(): void }>`
   left: 0;
   bottom: 0;
   background: rgba(0, 0, 0, 0.4);
-`
+`;
 
 const DrawerContent = styled.div`
   z-index: 1400;
   height: 100vh;
   width: 100%;
   overflow-x: hidden;
-`
+`;
 
 interface State {
-  animating: boolean
+  animating: boolean;
 }
 
 class Drawer extends React.Component<DrawerProps, State> {
-  state: State = { animating: false }
+  state: State = { animating: false };
 
   componentDidUpdate(pp: DrawerProps, ps: State) {
-    if (!pp.open && this.props.open) this.setState({ animating: true })
+    if (!pp.open && this.props.open) this.setState({ animating: true });
   }
 
   render() {
-    const { width, open, handleClose, children } = this.props
+    const { width, open, handleClose, children } = this.props;
     return (
       <>
-        <Motion
-          defaultStyle={{ x: 300, opacity: 1 }}
-          style={{ x: spring(open ? 0 : 300), opacity: spring(open ? 1 : 0) }}
-          onRest={() => this.setState({ animating: false })}
+        <Transition
+          items={open}
+          from={{ opacity: 0 }}
+          enter={{ opacity: 1 }}
+          leave={{ opacity: 0 }}
         >
-          {style => (
-            <>
-              {(this.props.open || this.state.animating) && (
-                <DrawerOverlay
-                  style={{
-                    opacity: style.opacity,
-                  }}
-                  onClick={handleClose}
-                />
-              )}
-              <DrawerWrapper
-                style={{
-                  transform: `translateX(${style.x}px)`,
-                }}
-                width={width}
-              >
+          {open => props =>
+            open && <DrawerOverlay style={props} onClick={handleClose} />}
+        </Transition>
+        <Transition
+          items={open}
+          from={{ transform: "translate3d(300px,0,0)" }}
+          enter={{ transform: "translate3d(0,0,0)" }}
+          leave={{ transform: "translate3d(300px,0,0)" }}
+        >
+          {open => props =>
+            open && (
+              <DrawerWrapper width={width} style={props}>
                 <DrawerContent>{children}</DrawerContent>
               </DrawerWrapper>
-            </>
-          )}
-        </Motion>
+            )}
+        </Transition>
       </>
-    )
+    );
   }
 }
 
-export { Drawer }
+export { Drawer };
