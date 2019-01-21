d285253b2190da8a76446465b94ea2d3a4e5fd22
Pawel Lisewski <pawel.lisewski@gmail.com>
2019-01-21 18:26:28 +0800

    add gatsby-image polyfill

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -5,14 +5,31 @@ interface ImgProps extends GatsbyImageProps {
   fixed?: any;
   fluid?: any;
   className?: string;
+  imgStyle?: object;
+  objFit?: string;
+  objPosition?: string;
 }
 
 const Image: React.SFC<ImgProps> = (
-  { fixed, fluid, className, style },
+  {
+    imgStyle,
+    objFit = "cover",
+    objPosition = "50% 50%",
+    fixed,
+    fluid,
+    className,
+    style,
+  },
   ...props
 ) => {
   return (
     <GatsbyImage
+      imgStyle={{
+        ...imgStyle,
+        objectFit: objFit,
+        objectPosition: objPosition,
+        fontFamily: `"object-fit: ${objFit}; object-position: ${objPosition}"`,
+      }}
       style={style}
       fluid={fluid && fluid.childImageSharp.fluid}
       fixed={fixed && fixed.childImageSharp.fixed}
